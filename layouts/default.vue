8e709ceb323239f9977922a70ebcd97cde15995b
Nidhi Kulkarni <nidhi.kulkarni@in.ibm.com>
2019-06-26 13:55:06 +0530

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,203 @@
+<template>
+   <v-app light>
+    <v-toolbar class="white">
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <section>
+        <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" height="600">
+          <v-layout
+            column
+            align-center
+            justify-center
+            class="white--text"
+          >
+            <h1 class="white--text mb-2 display-1 text-xs-center">Share to save</h1>
+            <div class="subheading mb-3 text-xs-center">A citizen's movement</div>
+          </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-layout
+          column
+          wrap
+          class="my-5"
+          align-center
+        >
+          <v-flex xs12 sm4 class="my-3">
+            <div class="text-xs-center">
+              <h2 class="headline">The best way to clean up the planet is to start from home</h2>
+              <span class="subheading">
+                Here's how
+              </span>
+            </div>
+          </v-flex>
+          <v-flex xs12>
+            <v-container grid-list-xl>
+              <v-layout row wrap align-center>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">delete</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Reduce carbon footprint</div>
+                    </v-card-title>
+                    <v-card-text>
+                     By being part of share to save, you save tonnes of one time disposable cutlery from ending up
+                     in the landfill. 
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">favorite</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline">Community</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Accumulate good karma and pay forward by spreading the word. Encourage people around you to 
+                      healthy practices for the community. 
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">cake</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Have fun without guilt</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Enjoy guilt free social gatherings by generating zero waste. Parties last for a few hours 
+                      but plastic lasts forever. 
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-flex>
+        </v-layout>
+      </section>
+
+      <section>
+        <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" height="380">
+          <v-layout column align-center justify-center>
+              <v-flex xs12 sm4 offset-sm1>
+              <v-card class="elevation-0 transparent">
+                <v-card-title primary-title class="layout justify-center">
+            <div class="headline white--text mb-3 text-xs-center">What is "Share to save"?</div>
+                </v-card-title>
+                <v-card-text class="white--text text-center">
+                  <div>Share to save is utensil bank that can be borrowed for small social gatherings.</div>
+                </v-card-text>
+                <v-list class="transparent white--text text-center justify-center">
+                  <v-subheader class="white--text text-center justify-center">
+                    Here is how it works:
+                  </v-subheader>
+                  <v-list-tile>
+                    <v-list-tile-content>
+                      <v-list-tile-title>Borrow the utensils</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content>
+                      <v-list-tile-title>Use them responsibly</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content>
+                      <v-list-tile-title>Clean them</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content>
+                      <v-list-tile-title>Return them on time</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+
+                </v-list>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-container grid-list-xl>
+          <v-layout row wrap justify-center class="my-5">
+            <v-flex xs12 sm4>
+              <v-card class="elevation-0 transparent">
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Motivation behind the initiative</div>
+                </v-card-title>
+                <v-card-text>
+                  Inspired by Nipun Mehta's Karma kitchen, Share to save tries to encourage people to 
+                  pay their dues with kindness to others rather than with money.
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 sm4 offset-sm1>
+              <v-card class="elevation-0 transparent">
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Contact us</div>
+                </v-card-title>
+                <v-card-text>
+                  Currently active in Pune. We have four active custodians
+                </v-card-text>
+                <v-list class="transparent">
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon class="blue--text text--lighten-2">phone</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>XXXXX</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon class="blue--text text--lighten-2">place</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>Pune, India</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon class="blue--text text--lighten-2">email</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>XXXX</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </section>
+
+      <v-footer class="blue darken-2">
+        <v-layout row wrap align-center>
+          <v-flex xs12>
+            <div class="white--text ml-3">
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-footer>
+    </v-content>
+  </v-app>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        title: 'Share to save'
+      }
+    }
+  }
+</script>
